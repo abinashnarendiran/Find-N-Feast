e6b4590c1c44a54055a94f99640653286d1c206f
100589716 <saijeeshan.ketheeswaran@uoit.net>
2018-10-07 13:50:55 -0400

    Added Signup Page

--- app/src/main/java/ca/csci4100/uoit/project/SignUpActivity.java
+++ app/src/main/java/ca/csci4100/uoit/project/SignUpActivity.java
@@ -3,11 +3,10 @@ package ca.csci4100.uoit.project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
     }
 }
