54f6beba77530364a2a8a7892d53dbb2d38e3dfa
abinashnarendiran <abinash.narendiran@uoit.net>
2018-10-08 14:04:55 -0400

    Add Intents

---
+++ app/src/main/java/ca/csci4100/uoit/project/InfoActivity.java
@@ -0,0 +1,23 @@
+package ca.csci4100.uoit.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+
+    public void backInfo(View view) {
+        Intent backInfoIntent = new Intent(
+                InfoActivity.this,
+                LoginActivity.class
+        );
+        startActivity(backInfoIntent);
+    }
+}
--- app/src/main/java/ca/csci4100/uoit/project/LoginActivity.java
+++ app/src/main/java/ca/csci4100/uoit/project/LoginActivity.java
@@ -1,7 +1,9 @@
 package ca.csci4100.uoit.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void getInfo(View view) {
+        Intent getInfoIntent = new Intent(
+                LoginActivity.this,
+                InfoActivity.class
+        );
+        startActivity(getInfoIntent);
+    }
+
+    public void signup(View view) {
+        Intent getSignupIntent = new Intent(
+                LoginActivity.this,
+                SignUpActivity.class
+        );
+        startActivity(getSignupIntent);
     }
 }
