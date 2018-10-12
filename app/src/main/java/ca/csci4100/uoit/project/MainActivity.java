b9f02019cc8e83f637acd711502ff7980c0f1c80
100589716 <saijeeshan.ketheeswaran@uoit.net>
2018-10-12 19:57:05 -0400

    Added Sliding Menu to Main Menu

--- app/src/main/java/ca/csci4100/uoit/project/MainActivity.java
+++ app/src/main/java/ca/csci4100/uoit/project/MainActivity.java
@@ -1,13 +1,31 @@
 package ca.csci4100.uoit.project;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle mToggle;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpToolbar();
     }
+
+    private void setUpToolbar() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+    }
+
 }
