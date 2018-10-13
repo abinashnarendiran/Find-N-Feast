722efdeb0b72781820c9c52008fb832fc951da93
abinashnarendiran <abinash.narendiran@uoit.net>
2018-10-13 14:55:42 -0400

    Temporary Login and Text Change Commit

--- app/src/main/java/ca/csci4100/uoit/project/LoginActivity.java
+++ app/src/main/java/ca/csci4100/uoit/project/LoginActivity.java
@@ -1,9 +1,12 @@
 package ca.csci4100.uoit.project;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,4 +31,20 @@ public class LoginActivity extends AppCompatActivity {
         );
         startActivity(getSignupIntent);
     }
+
+    public void login(View view) {
+        EditText email = (EditText) findViewById(R.id.email_input);
+        EditText password = (EditText) findViewById(R.id.password_input);
+
+        if (email.getText().toString().equals("A@gmail.com") && password.getText().toString().equals("A")) {
+            Intent LoginIntent = new Intent(this, MainActivity.class);
+            startActivity(LoginIntent);
+
+        }
+        else {
+            Toast toast2 = Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT);
+            toast2.show();
+        }
+
+        }
 }
--- app/src/main/java/ca/csci4100/uoit/project/MainActivity.java
+++ app/src/main/java/ca/csci4100/uoit/project/MainActivity.java
@@ -1,16 +1,20 @@
 package ca.csci4100.uoit.project;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle mToggle;
     Toolbar toolbar;
+    public static int GET_LOGIN_INTENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
         mToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
         drawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent resultIntent) {
+        if(requestCode == GET_LOGIN_INTENT) {
+            if(resultCode == Activity.RESULT_OK){
+                //correct password
+                Toast toast = Toast.makeText(MainActivity.this, " Login Success", Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        }
+        // TODO: handle the province select event
     }
 
 }
